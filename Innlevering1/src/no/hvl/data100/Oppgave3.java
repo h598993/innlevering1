20a022c9937d6685c87ac17006a5635233066be0
stian <stian@MSI.lan>
2021-09-06 22:45:08 +0200

    Tredje oppgave lagt til

---
+++ Innlevering1/src/no/hvl/data100/Oppgave3.java
@@ -0,0 +1,30 @@
+package no.hvl.data100;
+
+import static javax.swing.JOptionPane.*;
+
+public class Oppgave3 {
+
+	public static void main(String[] args) {
+
+		// Tar inn brukerinput
+
+		int brukerInput = Integer.parseInt(showInputDialog("Skriv inn et tall"));
+
+		while (brukerInput <= 0) {
+			brukerInput = Integer
+					.parseInt(showInputDialog("Ugyldig tall. Tallet må være større enn null. Prøv Igjen!"));
+		}
+
+		// Regner n-fakultetet og viser svaret i et dialogvindu
+
+		long sum = 1;
+
+		for (int i = 1; i <= brukerInput; i++) {
+			sum *= i;
+		}
+
+		showMessageDialog(null, "n! til " + brukerInput + " er " + sum);
+
+	}
+
+}
